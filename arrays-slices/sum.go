64b0ecd3e64b6ab1522f9476166fc662e387e1c5
noah hetrick <noah.hetrick@onxmaps.com>
2025-12-30 10:42:58 -0500

    sum slices

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -1,9 +1,33 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+
+	return sums
 }
--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,14 +1,41 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
+	result := Sum(numbers)
+	expected := 15
 
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d, wanted %d given %v", got, want, numbers)
+	if result != expected {
+		t.Errorf("got %d, but expected %d, given %v", result, expected, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	result := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+	expected := []int{6, 15}
+
+	if !slices.Equal(result, expected) {
+		t.Errorf("got %v, but expected %v", result, expected)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, result, expected []int) {
+		t.Helper()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("got %v, but expected %v", result, expected)
+		}
+	}
+	t.Run("sum tails", func(t *testing.T) {
+		checkSums(t, SumAllTails([]int{1, 2}, []int{3, 4, 5}), []int{2, 9})
+	})
+	t.Run("sum tails with empty slice", func(t *testing.T) {
+		checkSums(t, SumAllTails([]int{}, []int{1, 2, 3}), []int{0, 6})
+	})
 }
